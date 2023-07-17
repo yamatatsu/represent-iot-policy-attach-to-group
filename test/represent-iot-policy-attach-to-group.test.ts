8cba65f4cc43621982d6538af709b1cc408b3644
yamatatsu <the.kinnikuman@gmail.com>
2023-07-17 17:54:13 +0900

    Initial commit

---
+++ test/represent-iot-policy-attach-to-group.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RepresentIotPolicyAttachToGroup from '../lib/represent-iot-policy-attach-to-group-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/represent-iot-policy-attach-to-group-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RepresentIotPolicyAttachToGroup.RepresentIotPolicyAttachToGroupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
